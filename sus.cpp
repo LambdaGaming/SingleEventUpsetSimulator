09d5d3eb2d2e12f71283c145d5c57a4cdd146b97
LambdaGaming <starwars18m@gmail.com>
2023-11-17 02:40:43 -0500

    Add ability to flip select number of bits

--- sus.cpp
+++ sus.cpp
@@ -21,13 +21,13 @@ void EnableDebugPerms()
     LUID id;
     if ( !OpenProcessToken( GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &token ) )
     {
-        cerr << "Couldn't open process token." << endl;
+        cerr << "Couldn't open process token. Aborting..." << endl;
         system( "pause" );
         exit( 1 );
     }
     if ( !LookupPrivilegeValue( nullptr, SE_DEBUG_NAME, &id ) )
     {
-        cerr << "Couldn't lookup privilege value." << endl;
+        cerr << "Couldn't lookup privilege value. Aborting..." << endl;
         system( "pause" );
         exit( 1 );
     }
@@ -38,7 +38,7 @@ void EnableDebugPerms()
     priv.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
     if ( !AdjustTokenPrivileges( token, FALSE, &priv, sizeof( TOKEN_PRIVILEGES ), nullptr, nullptr ) )
     {
-        cerr << "Couldn't adjust privileges." << endl;
+        cerr << "Couldn't adjust privileges. Aborting..." << endl;
         system( "pause" );
         exit( 1 );
     }
@@ -78,11 +78,12 @@ unsigned char* GetRandomAddress( HANDLE proc )
     }
     if ( pages.size() == 0 )
     {
-        cerr << "Couldn't find any valid memory addresses for selected process." << endl;
+        cerr << "Couldn't find any valid memory addresses for selected process. Aborting..." << endl;
         system( "pause" );
         exit( 1 );
     }
 
+    // Get a truly random address by picking one between the start and end of the page
     random_device rd;
     mt19937 gen( rd() );
     Memory randomMem = pages[rand() % pages.size()];
@@ -98,43 +99,57 @@ void FlipRandomBit( BYTE &data )
     data ^= ( 1 << random ); // Invert random bit using bitwise XOR
 }
 
-int main()
+void RunMemoryManipulation()
 {
-    // Make sure we have the right permissions to be doing all this
-    EnableDebugPerms();
-
     // Generate a list of currently running processes and pick a random one
-    srand( time( NULL ) );
+Start:
     DWORD procId = GetRandomProcess();
     cout << "Opening process " << procId << endl;
     HANDLE proc = OpenProcess( PROCESS_ALL_ACCESS, false, procId );
     if ( !proc )
     {
-        cerr << "Couldn't open process " << procId << ". It's either protected by Windows or the architectures don't match." << endl;
-        system( "pause" );
-        exit( 1 );
+        cerr << "Couldn't open process " << procId << ". It's either protected by Windows or the architectures don't match. Retrying..." << endl;
+        goto Start;
     }
 
     // Get a list of memory addresses from the selected process that actually have data, then read a random one from that list
     BYTE buffer;
     PVOID addr = GetRandomAddress( proc );
-    if ( !ReadProcessMemory( proc, addr, &buffer, sizeof( buffer ), NULL) )
+    if ( !ReadProcessMemory( proc, addr, &buffer, sizeof( buffer ), NULL ) )
     {
-        cerr << "Couldn't read memory address 0x" << addr << "." << endl;
-        system( "pause" );
-        exit( 1 );
+        cerr << "Couldn't read memory address 0x" << addr << ". Retrying..." << endl;
+        goto Start;
     }
 
     // Flip random bit and write it to memory
     cout << "Old data at address 0x" << addr << ": 0x" << hex << uppercase << int( buffer ) << endl;
     FlipRandomBit( buffer );
-    if ( !WriteProcessMemory( proc, addr, &buffer, sizeof( buffer ), NULL) )
+    if ( !WriteProcessMemory( proc, addr, &buffer, sizeof( buffer ), NULL ) )
     {
-        cerr << "Couldn't write to memory address 0x" << addr << "." << endl;
-        system( "pause" );
-        exit( 1 );
+        cerr << "Couldn't write to memory address 0x" << addr << ". Retrying..." << endl;
+        goto Start;
     }
-    cout << "New data at address 0x" << addr << ": 0x" << hex << uppercase << int( buffer ) << endl;
+    cout << "New data at address 0x" << addr << ": 0x" << hex << uppercase << int( buffer ) << dec << endl;
     CloseHandle( proc );
+}
+
+int main()
+{
+    int amount = 0;
+    cout << "Single-event Upset Simulator (SUS) v1.0 | Copyright (c) 2023 LambdaGaming" << endl;
+    cout << "WARNING!!! THIS PROGRAM EDITS RANDOM BITS OF MEMORY OF RANDOM PROCESSES, WHICH CAN SERIOUSLY SCREW UP YOUR SYSTEM! PROCEED AT YOUR OWN RISK!" << endl << endl;
+    cout << "Enter the amount of random bits you want to flip: ";
+    cin >> amount;
+    srand( time( NULL ) );
+
+    // Make sure we have the right permissions to be doing all this
+    EnableDebugPerms();
+
+    for ( int i = 0; i < amount; i++ )
+    {
+        RunMemoryManipulation();
+    }
+
+    cout << "Process completed." << endl;
     system( "pause" );
 }
